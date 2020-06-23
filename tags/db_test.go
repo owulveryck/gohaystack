49539f861910a515f63460e0b4dd133ae630e71e
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2020-06-23 12:15:03 +0200

    feat: one more convenient method

--- tags/db_test.go
+++ tags/db_test.go
@@ -46,3 +46,54 @@ func TestGetLabelByName(t *testing.T) {
 		})
 	}
 }
+
+func TestGetMarkerByName(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  func() (*gohaystack.Label, *gohaystack.Value)
+		want1 bool
+	}{
+		{
+			"found",
+			args{
+				"site",
+			},
+			markerDB["site"],
+			true,
+		},
+		{
+			"not found",
+			args{
+				"NOTFOUND",
+			},
+			nil,
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := GetMarkerByName(tt.args.name)
+			if got != nil && tt.want != nil {
+				gotL, gotV := got()
+				wantL, wantV := tt.want()
+				if !reflect.DeepEqual(gotL, wantL) {
+					t.Errorf("GetMarkerByName()")
+				}
+				if !reflect.DeepEqual(gotV, wantV) {
+					t.Errorf("GetMarkerByName()")
+				}
+			} else {
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("GetMarkerByName()")
+				}
+			}
+			if got1 != tt.want1 {
+				t.Errorf("GetMarkerByName() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
