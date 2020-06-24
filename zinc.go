c57cafa28fd3eec8caf680bc8504786f2296b884
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2020-06-24 16:53:56 +0200

    feat(wip): zinc marshaler skeleton

---
+++ zinc.go
@@ -0,0 +1,103 @@
+package gohaystack
+
+import (
+	"errors"
+	"strings"
+)
+
+// MarshalZinc encode the grrind in zinc format.
+// https://www.project-haystack.org/doc/Zinc
+func (g *Grid) MarshalZinc() ([]byte, error) {
+	var b strings.Builder
+	err := g.marshalMetaZinc(&b)
+	if err != nil {
+		return nil, err
+	}
+	// Writing columns
+	labels := g.marshalColsZinc(&b)
+	// Values
+	i := 0
+	for _, entity := range g.entities {
+		for _, l := range labels {
+			if v, ok := entity.GetTags()[l]; ok {
+				z, err := v.MarshalZinc()
+				if err != nil {
+					return nil, err
+				}
+				b.Write(z)
+			}
+
+			if i < len(labels)-1 && i != 0 {
+				b.WriteString(`,`)
+			}
+			i++
+		}
+		b.WriteString(`\n`)
+	}
+
+	return []byte(b.String()), nil
+}
+
+// MarshalZinc for a value
+func (v *Value) MarshalZinc() ([]byte, error) {
+	if v == nil {
+		return nil, errors.New("cannot marshal this type")
+	}
+	return nil, nil
+}
+
+func (g *Grid) marshalMetaZinc(b *strings.Builder) error {
+	var hasVer bool
+	var version string
+	if v, ok := g.Meta["Ver"]; ok {
+		hasVer = true
+		version = v
+	}
+	if v, ok := g.Meta["ver"]; ok {
+		hasVer = true
+		version = v
+	}
+	if !hasVer {
+		return errors.New("Bad formatting, missing version tag")
+	}
+	if version != "3.0" {
+		return errors.New("Unsupported version " + version)
+	}
+	// Writing header
+	b.WriteString(`ver:"3.0"`)
+	for t, v := range g.Meta {
+		if t == "ver" || t == "Ver" {
+			continue
+		}
+		b.WriteString(` ` + t + `:`)
+		b.WriteString(`"` + v + `"`)
+	}
+	b.WriteString(`\n`)
+	return nil
+}
+
+func (g *Grid) marshalColsZinc(b *strings.Builder) []*Label {
+	labelsDic := make(map[*Label]struct{}, 0)
+	for _, entity := range g.entities {
+		for label := range entity.tags {
+			labelsDic[label] = struct{}{}
+		}
+	}
+	labels := make([]*Label, 0, len(labelsDic))
+	i := 0
+	for l := range labelsDic {
+		b.WriteString(l.Value)
+		if l.Display != "" {
+			b.WriteString(` dis:"` + l.Display + `"`)
+		}
+		labels = append(labels, l)
+		if i < len(labelsDic)-1 {
+			b.WriteString(`,`)
+		}
+		i++
+	}
+	if len(labels) > 0 {
+		b.WriteString(`\n`)
+	}
+	return labels
+}
---
+++ zinc_test.go
@@ -0,0 +1,194 @@
+package gohaystack
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGrid_MarshalZinc(t *testing.T) {
+	//	blabla := "blabla"
+	myID := NewHaystackID("myid")
+	type fields struct {
+		Meta     map[string]string
+		entities []*Entity
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []byte
+		wantErr bool
+	}{
+		{
+			"empty grid",
+			fields{},
+			nil,
+			true,
+		},
+		{
+			"bad meta",
+			fields{
+				Meta: map[string]string{
+					"version": "3.0",
+				},
+			},
+			nil,
+			true,
+		},
+		{
+			"good meta, bad version",
+			fields{
+				Meta: map[string]string{
+					"ver": "4.0",
+				},
+			},
+			nil,
+			true,
+		},
+		{
+			"good meta, bar Version",
+			fields{
+				Meta: map[string]string{
+					"Ver": "4.0",
+				},
+			},
+			nil,
+			true,
+		},
+		{
+			"good meta, good Version",
+			fields{
+				Meta: map[string]string{
+					"ver": "3.0",
+				},
+			},
+			[]byte(`ver:"3.0"\n`),
+			false,
+		},
+		{
+			"good meta, good Version, more meta",
+			fields{
+				Meta: map[string]string{
+					"ver":      "3.0",
+					"database": "test",
+				},
+			},
+			[]byte(`ver:"3.0" database:"test"\n`),
+			false,
+		},
+		{
+			"one column",
+			fields{
+				Meta: map[string]string{
+					"ver":      "3.0",
+					"database": "test",
+				},
+				entities: []*Entity{
+					{
+						id: myID,
+						tags: map[*Label]*Value{
+							{Value: "blabla"}: {
+								kind: HaystackLastType,
+							},
+						},
+					},
+				},
+			},
+			[]byte(`ver:"3.0" database:"test"\nblabla\n\n`),
+			false,
+		},
+		{
+			"one column and display",
+			fields{
+				Meta: map[string]string{
+					"ver":      "3.0",
+					"database": "test",
+				},
+				entities: []*Entity{
+					{
+						id: myID,
+						tags: map[*Label]*Value{
+							{
+								Value:   "blabla",
+								Display: "display",
+							}: {
+								kind: HaystackLastType,
+							},
+						},
+					},
+				},
+			},
+			[]byte(`ver:"3.0" database:"test"\nblabla dis:"display"\n\n`),
+			false,
+		},
+		{
+			"two columns and display",
+			fields{
+				Meta: map[string]string{
+					"ver":      "3.0",
+					"database": "test",
+				},
+				entities: []*Entity{
+					{
+						id: myID,
+						tags: map[*Label]*Value{
+							{
+								Value: "a",
+							}: {
+								kind: HaystackLastType,
+							},
+							{
+								Value: "a",
+							}: {
+								kind: HaystackLastType,
+							},
+						},
+					},
+				},
+			},
+			[]byte(`ver:"3.0" database:"test"\na,a\n\n`), // This should not be valid, but in the grid the two labels are differents
+			false,
+		},
+		{
+			"bad valud",
+			fields{
+				Meta: map[string]string{
+					"ver":      "3.0",
+					"database": "test",
+				},
+				entities: []*Entity{
+					{
+						id: myID,
+						tags: map[*Label]*Value{
+							{
+								Value: "a",
+							}: nil,
+							{
+								Value: "a",
+							}: {
+								kind: HaystackLastType,
+							},
+						},
+					},
+				},
+			},
+			nil,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &Grid{
+				Meta:     tt.fields.Meta,
+				entities: tt.fields.entities,
+			}
+			got, err := g.MarshalZinc()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Grid.MarshalZinc() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Grid.MarshalZinc() = %v, want %v", string(got), string(tt.want))
+			}
+		})
+	}
+}
